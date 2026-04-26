ac56ddaf0134f6e077ef924c7899543a87a0b5c2
beneducarrco99 <bcarrascoco@it.ucsc.cl>
2026-04-26 18:38:13 -0400

    versionPrototipoInicialCHECK

--- README.md
+++ README.md
@@ -3,10 +3,34 @@ Benjammín Carrasco Concha.   20.022.665-8   23/04/2026
 
 En esta sección, encontrará el prototipo inicial de un sistema computacional diseñado y desarrollado para universidades y sus procesos académicos y administrativos.
 
-Diagrama de clases.
+Diagrama de clases:
 
 1. El desarrollo del diagrama comenzó definiendo las clases usuarios: clase Docente y clase Estudiante. Para optimizar la contaminación de atributos y métodos, creé una superclase llamada "Persona" que tomarará los datos compartidos entre ambos usuarios. También, definí sus relaciones de herencia, las relaciones están declaradas al final del código del diagrama.
 
 2. Seguimos con la definición de las clases "Curso" y "Asignatura" para continuar con sus relaciones y aprovechamos de enlazar relaciones con las clases usuarios. Un Docente puede impartir más de una asignatura y puede enseñar en más de un curso, así como una asignatura, dependiendo del curso y período, puede ser impartida por más de un Docente.
 
 3. Por último, definimos dos clases administrativas iniciales: clase Arancel y clase Beca. Pulimos y creamos las últimas relaciones y métodos.
+
+Estructura código:
+
+- El código es la primera versión del sistema y se experimenta con algunas funciones básicas e iniciales.
+- Todas las clases están declaradas en el programa.
+- Hay herencia entre la superclase Persona y sus subclases Docente y Estudiante.
+- En la familia de clases que heredan, hay polimorfismo en el método logearUsuario().
+- El encapsulamiento está diseñado a las necesidades que requiere cada clase.
+- La separación entre clases está bien señalizada.
+- Hay comentarios en las partes más complejas del código para guiar a los demás desarrolladores.
+
+Instrucciones de ejecución:
+
+1. Primero iniciarás sesión como estudiante e inscribirás un curso.
+2. Luego, iniciarás sesión como docente y decidirás cuantas notas ingresarás.
+3. Aún como docente, probarás el ingreso de notas y te retornará el promedio junto con el mensaje de aprobación o reprobación.
+
+Tecnologías usadas:
+
+- Visual Studio Code
+- PlantUML en VS Code
+- Lenguaje Python
+- Controlador de versiones GIT
+----------------------------------------------------------------------------------------------------------------------------------
--- evaluacion4POO.py
+++ evaluacion4POO.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+# --------------------------------------------------------------------------------------------------------------------------
+
 class Persona:
     def __init__(self, rut, nombre, apellidoP, apellidoM):
         self._rut = rut
@@ -8,11 +10,21 @@ class Persona:
         self._apellidoM = apellidoM
         self._fechaNac = datetime.now()
 
-    def logearUsuario(self):
+
+    def logearUsuario(self):                                                 # Logeo Default
         print("------ Iniciar sesión ------")
         self._rut = str(input("Ingrese RUT: "))
         self._nombre = str(input("Ingrese Nombre: "))
 
+
+    def verCalificacion(self):
+        pass
+
+
+    def mensajear(self):
+        pass
+
+# --------------------------------------------------------------------------------------------------------------------------
 
 class Docente (Persona):
     def __init__(self, rut, nombre, apellidoP, apellidoM, asignatura):
@@ -22,7 +34,9 @@ class Docente (Persona):
         self.__notasSemestre = 0
         self.__contador = 0
 
-    def logearUsuario(self):
+
+    def logearUsuario(self):                                                 # Logeo Docente
+        print("--------------------------------------------------------------------------------------------------------------")
         print("°°°    Sección Docente   °°°")
         print("------ Iniciar sesión ------")
         self._rut = str(input("Ingrese RUT: "))
@@ -30,11 +44,15 @@ class Docente (Persona):
         print(f"Bienvenido profesor/a {self._nombre}")
         self.adjuntarMaterial()
 
-    def adjuntarMaterial(self):
+
+    def adjuntarMaterial(self):   # Acá Docente inicia la asignatura del semeste. Puede estipular la cantidad de evaluaciones por ejemplo.
+        print("--------------------------------------------------------------------------------------------------------------")
         self.__notasSemestre = int(input("Antes de comenzar actividades. ¿Cuántas notas ingresará este semestre? "))
         self.registrarCalificacion()
 
-    def registrarCalificacion(self):
+
+    def registrarCalificacion(self):   # Acá Docente ingresará las notas de los Estudiantes.
+        print("--------------------------------------------------------------------------------------------------------------")
         while self.__contador < self.__notasSemestre:
             self.__nota = int(input("Ingrese nota: "))
             
@@ -43,11 +61,12 @@ class Docente (Persona):
                 return
             
             self.__contador += 1
-            self.__asignatura.almacenaNotas(self.__nota, self.__notasSemestre)
-        
+            self.__asignatura.compartirInfo(self.__nota, self.__notasSemestre) # Llamamos a la instancia de Asignatura y a su método de información
         print("Ya ingresó el número máximo de notas en este semestre.")
-        self.__asignatura.promedioNotas()
+        
+        self.__asignatura.promedioNotas()   # Llamamos a la instancia de Asignatura y a su método de promedio
 
+# --------------------------------------------------------------------------------------------------------------------------
 
 class Estudiante (Persona):
     def __init__(self, rut, nombre, apellidoP, apellidoM, carrera):
@@ -57,18 +76,39 @@ class Estudiante (Persona):
         self.__beneficio = False
         self.__inscribe = ""
 
-    def logearUsuario(self):
+
+    def logearUsuario(self):                                                 # Logeo Estudiante
+        print("--------------------------------------------------------------------------------------------------------------")
         print("°°°    Sección Alumno   °°°")
         print("------ Iniciar sesión ------")
         self._rut = str(input("Ingrese RUT: "))
         self._nombre = str(input("Ingrese Nombre: "))
+        print("--------------------------------------------------------------------------------------------------------------")
         print(f"Bienvenido alumno/a {self._nombre}")
         self.inscribirCurso()
 
-    def inscribirCurso(self):
-        self.__inscribe = str(input("Inscriba una carrera: "))
-        self.__carrera.sumarMatri(self.__inscribe)
 
+    def inscribirCurso(self):   # Acá Estudiante inscribe un curso/carrera. Prototipo inicial con margen de mejora.
+        self.__inscribe = str(input("Inscriba una carrera: "))
+        self.__carrera.sumarMatri(self.__inscribe) # Llamamos a la instancia de Curso y a su método de matriculados.
+
+
+    def visualizarMaterial(self):
+        pass
+
+
+    def desarrollarEvaluacion(self):
+        pass
+
+
+    def adjuntarEvaluacion(self):
+        pass
+
+
+    def solicitarBeneficio(self):
+        pass
+
+# --------------------------------------------------------------------------------------------------------------------------
 
 class Curso:
     def __init__(self, departamento, profJefe, duracion, periodo, cantidadMatri):
@@ -80,12 +120,23 @@ class Curso:
         self.__cantidadMatri = cantidadMatri
         self.__nroMatri = 0
 
-    def sumarMatri(self, cantidadMatri):
+
+    def asignarJefeCarr(self):
+        pass
+    
+    
+    def registrarPeriodo(self):
+        pass
+
+
+    def sumarMatri(self, cantidadMatri): # Registra carrera y suma matriculados. Prototipo inicial con margen de mejora.
         self.__cantidadMatri = cantidadMatri
         self.__nroMatri += 1
 
+        print("--------------------------------------------------------------------------------------------------------------")
         print(f"Para la carrera de {self.__cantidadMatri} hay un total de {self.__nroMatri} alumnos inscritos")
 
+# --------------------------------------------------------------------------------------------------------------------------
 
 class Asignatura:
     def __init__(self, semestre, departamentoAsig, profesor, nota, notasSemestre):
@@ -97,21 +148,30 @@ class Asignatura:
         self.__promedio = 0
         self.__cantidadNotas = 0
 
-    def almacenaNotas(self, nota, notasSemestre):
-        #if (self.__cantidadNotas < self.__notasSemestre):
+
+    def asignarProfe(self):
+        pass
+    
+    
+    def compartirInfo(self, nota, notasSemestre):   # Prototipo inicial con margen de mejora de la información semestral de la asignatura, de momento solo gestiona notas.
         self.__notasSemestre = notasSemestre 
         self.__nota += nota
         self.__cantidadNotas = self.__cantidadNotas + 1
         self.__promedio = self.__nota / self.__cantidadNotas
     
-    def promedioNotas(self):
+
+    def promedioNotas(self):   # Muestra promedio de nota para el Estudiante
+        print("--------------------------------------------------------------------------------------------------------------")
         print(f"{self.__promedio}")
 
         if (self.__promedio >= 4):
             print(f"El alumno/a aprobó la asignatura con promedio {self.__promedio}")
         else:
             print(f"El alumno/a reprobó la asignatura con promedio {self.__promedio}")
+        
+        print("--------------------------------------------------------------------------------------------------------------")
 
+# --------------------------------------------------------------------------------------------------------------------------
 
 class Arancel:
     def __init__(self, anio, valor, cuota):
@@ -120,11 +180,25 @@ class Arancel:
         self.__cuota = cuota
 
 
+    def calcularValorCuota(self):
+        pass
+
+
+    def mostrarCuota(self):
+        pass
+
+# --------------------------------------------------------------------------------------------------------------------------
+
 class Beca:
     def __init__(self, nombreBec, cantFinan):
         self.__nombreBec = nombreBec
         self.__cantFinan = cantFinan
 
+
+    def calcularRequisitos(self):
+        pass
+
+# --------------------------------------------------------------------------------------------------------------------------
 
 curso = Curso("", "", 0, "", "")
 novato = Estudiante("", "", "", "", curso)
